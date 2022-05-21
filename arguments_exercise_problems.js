c9e679451f849606da91483a6b55c0f76c202f08
Kevin <NateraKevin1@gmail.com>
2022-05-20 22:56:28 -0400

    added my bind

--- arguments_exercise_problems.js
+++ arguments_exercise_problems.js
@@ -14,3 +14,35 @@ function sum(...args) {
   return total;
 }
 
+Function.prototype.myBind = function(context) {
+  const that = this;
+  const bindArgs = Array.from(arguments).slice(1);
+  return function() {
+    const callArgs = Array.from(arguments);
+
+    return that.apply(context, bindArgs.concat(callArgs))
+  }
+}
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  says(sound, person) {
+    console.log(`${this.name} says ${sound} to ${person}!`);
+    return true;
+  }
+}
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+const notMarkovSays = markov.says.myBind(pavlov);
+notMarkovSays("meow", "me");
